adf63bdcba40255ef7e4c83141cd69bc520b3150
Casian <louliett@gmail.com>
2020-01-31 21:38:48 +0100

    implemented the table, fetch command and basic style

--- work/report/js/main.js
+++ work/report/js/main.js
@@ -1,10 +1,35 @@
-/**
- * A function to wrap it all in.
- */
-(function () {
-    "use strict";
+var myData;
+
+fetch('data/skolenhet.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+
+        myData = data.Skolenheter;
 
 
+        if(myData.length > 0) {
 
-    console.log("All ready.");
-}());
+            var tempp = "";
+
+            myData.forEach((u) => {
+
+                tempp += "<tr>"
+                tempp += "<td>" + u.Skolenhetskod + "</td>";
+                tempp += "<td>" + u.Skolenhetsnamn + "</td>";
+                tempp += "<td>" + u.Kommunkod + "</td>";
+                tempp += "<td>" + u.PeOrgNr + "</td>";
+                tempp += "<tr>"
+
+
+            });
+
+            document.getElementById('school_table').innerHTML = tempp;
+
+
+        }
+
+
+    });
